1a55318d8126c225fc0eb2bb4f2657fa4a90996d
ma00hao <machenhao1996@vip.qq.com>
2020-07-20 23:02:27 +0800

    [adderss]:machenhao:地址查询修改

--- src/main/java/com/qf/dao/UserDao.java
+++ src/main/java/com/qf/dao/UserDao.java
@@ -1,13 +1,18 @@
 package com.qf.dao;
 
+import com.qf.pojo.User;
 import org.springframework.stereotype.Repository;
 
 /**
  * @ClassName: UserDao
- * @Author: 王赛
+ * @Author: 马陈浩
  * @Date: 2020/7/18
  * @Time: 11:22
  */
 @Repository
 public interface UserDao {
+    //修改user信息
+    int updateUser(User user);
+    //根据id查询用户信息
+    User selectUserById(int user_id);
 }
--- src/main/java/com/qf/service/UserService.java
+++ src/main/java/com/qf/service/UserService.java
@@ -1,15 +1,19 @@
 package com.qf.service;
 
 import com.qf.pojo.User;
+import com.qf.vo.R;
 
 import java.util.List;
 
 /**
  * @ClassName: UserService
- * @Author: 王赛
+ * @Author: 马陈浩
  * @Date: 2020/7/18
  * @Time: 11:23
  */
 public interface UserService {
-
+    //修改user信息
+    R updateUser(User user);
+    //根据id查询用户信息
+    R selectUserById(Integer user_id);
 }
