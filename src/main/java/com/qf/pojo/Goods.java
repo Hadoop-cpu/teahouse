fcff44042398e78defa1b6ff07811ca4ce593ac7
jpt-666 <739181116@qq.com>
2020-07-21 09:57:31 +0800

    [shoppingcar]jpt:完善购物车

--- src/main/java/com/qf/pojo/Goods.java
+++ src/main/java/com/qf/pojo/Goods.java
@@ -10,14 +10,18 @@ import lombok.Data;
  */
 @Data
 public class Goods {
+
     private Integer goods_id;           //商品id
     private String goods_name;          //商品名称
     private Integer goods_putaway;      //是否上架，0是1否
-    private Integer goods_total_num;    //商品总库存
-    private Integer goods_sales_volume; //商品总销量
     private String goods_info;          //商品详情
     private Integer goods_img;          //商品图片，关联图片表
-    private Integer goods_type;         //商品类型，关联商品类型表
     private Integer goods_brand;        //商品品牌、关联品牌表
     private Integer goods_variety;      //茶叶品种、关联品种表、若商品非茶叶商品，此字段可以为空
+    private Integer goods_type_id;      //商品类型，关联商品类型表
+    private Double goods_market_price;  //商品市场价
+    private Double goods_shop_price;    //商品商城价
+    private Integer goods_inventory;    //商品库存
+    private Integer goods_sales;        //商品销售额
+
 }
